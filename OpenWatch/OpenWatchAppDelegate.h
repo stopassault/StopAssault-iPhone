8ba6ee46572283530c08b4ff017c92f1fa869279
Micah Lee <micahflee@gmail.com>
2011-09-21 19:55:44 -0700

    Initial Commit

---
+++ OpenWatch/OpenWatchAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  OpenWatchAppDelegate.h
+//  OpenWatch
+//
+//  Created by micah on 9/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class OpenWatchViewController;
+
+@interface OpenWatchAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet OpenWatchViewController *viewController;
+
+@end
---
+++ OpenWatch/OpenWatchViewController.h
@@ -0,0 +1,13 @@
+//
+//  OpenWatchViewController.h
+//  OpenWatch
+//
+//  Created by micah on 9/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenWatchViewController : UIViewController
+
+@end
